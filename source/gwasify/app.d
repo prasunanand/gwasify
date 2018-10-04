2d2424cdc369a90f407680370898992eeb9ab96f
Prasun Anand <pa402238@us1salxhpg0004.corpnet2.com>
2018-10-04 06:09:52 -0400

    adapting to biobank snps

--- source/gwasify/app.d
+++ source/gwasify/app.d
@@ -29,6 +29,9 @@ void main(string[] args){
   else if(generator == "snps"){
     annotation_generator(file_name);
   }
+  else if(generator == "details"){
+    chr_details(file_name);
+  }
 }
 
 void annotation_generator(string file_name){
@@ -36,12 +39,36 @@ void annotation_generator(string file_name){
   File input = pipe.stdout;
 
   File anno_file =  File("snps.txt", "w");
+  File error_file =  File("error_log.txt", "w");
+  size_t index = 1;
   foreach(line; input.byLine){
-    auto chr = to!string(line).split(",")[0];
-    auto anno = chr.split(":");
-    anno_file.writeln(anno[0], "\t", anno[1], "\t", anno[2], anno[3]);
+    auto chr = to!string(line).split(" ")[0];
+    // writeln(chr);
+    //auto anno = chr[0].split(":");
+    //if(anno.length == 4){
+      anno_file.writeln(chr);
+    //}
+    //else{
+    //error_file.writeln(index, " => ", anno);
+    //}
+  index++;
   }
 
+}
+
+void chr_details(string file_name){
+  //auto pipe = pipeShell("gunzip -c " ~ file_name);
+  File input = File(file_name);
+
+  File chr_variants = File("variants.txt", "w");
+
+  foreach(line; input.byLine){
+    auto chr = to!string(line).split(",");
+    //writeln(chr);
+    auto chr_name = chr[0];
+    auto positions = chr[1].split(":");
+    chr_variants.writeln(chr_name, "\t", positions[0], "\t", positions[1]);
+  }
 }
 
 void pheno_covar_generator(string file_name, size_t mode, string pheno_cols, string covar_cols){
